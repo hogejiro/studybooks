2325b2d2d31a93bedaadb5a7cef34e71c10da9ee
hogejiro <combatechizen2@gmail.com>
2015-01-02 00:04:29 +0900

    modify bug @ problem 5-5

--- programming_in_haskell/05/05_two_comprehension_one_generator.hs
+++ programming_in_haskell/05/05_two_comprehension_one_generator.hs
@@ -1,8 +1,8 @@
 -- one comprehension, two generators
 -- list = [(x, y) | x <- [1,2,3], y <- [4,5,6]]
 
--- two comprehensions
-list = concat [[(x, y) | y <- [4, 5, 6], x <- [1, 2, 3]]]
+-- two comprehensions, one generator
+list = concat [[(x, y) | y <- [4, 5, 6]] | x <- [1, 2, 3]]
 
 -- main = putStrLn $ show $ list
--- [(1,4),(2,4),(3,4),(1,5),(2,5),(3,5),(1,6),(2,6),(3,6)]
+-- [(1,4),(1,5),(1,6),(2,4),(2,5),(2,6),(3,4),(3,5),(3,6)]
