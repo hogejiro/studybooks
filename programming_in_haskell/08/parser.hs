903b0c414239ebd72a01626e896edf09e68ba24d
hogejiro <combatechizen2@gmail.com>
2015-01-07 01:41:35 +0900

    add +++, sat, ..., string (but, this code can not run)

--- programming_in_haskell/08/parser.hs
+++ programming_in_haskell/08/parser.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 type Parser a = String -> [(a, String)]
 
 myreturn :: a -> Parser a
@@ -19,4 +21,36 @@ p >>>= f = \inp -> case parse p inp of
                     [] -> []
                     [(v, out)] -> parse (f v) out
 
+(+++) :: Parser a -> Parser a -> Parser a
+p +++ q = \inp -> case parse p inp of
+                    [] -> parse q inp
+                    [(v, out)] -> [(v, out)]
+
+sat :: (Char -> Bool) -> Parser Char
+sat p = do x <- item
+           if p x then myreturn x else failure
+
+lower :: Parser Char
+lower = sat isLower
+
+upper :: Parser Char
+upper = sat isUpper
+
+letter :: Parser Char
+letter = sat isAlpha
+
+alphanum :: Parser Char
+alphanum = sat isAlphaNum
+
+digit :: Parser Char
+digit = sat isDigit
+
+char :: Char -> Parser Char
+char x = sat (== x)
+
+string :: String -> Parser String
+string [] = return []
+string (x:xs) = do char x
+                   string xs
+                   myreturn (x:xs)
 
