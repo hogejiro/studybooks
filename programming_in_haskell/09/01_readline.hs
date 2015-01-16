1e0d974f8060c097b5df1f3328027a06a2b1f57d
hogejiro <combatechizen2@gmail.com>
2015-01-16 23:59:06 +0900

    unsolve exercise 1 @ chapter 9

---
+++ programming_in_haskell/09/01_readline.hs
@@ -0,0 +1,11 @@
+readLine :: IO String
+readLine xs = do x <- getChar
+                 case x of
+                   '\n'   -> return []
+                   '\DEL' -> if null xs
+                               then
+                                   readLine xs
+                               else
+                                   do putStr "\ESC[1D \ESC[1D"
+                                       readLine (init xs)
+                   _ -> readLine (xs ++ [x])
