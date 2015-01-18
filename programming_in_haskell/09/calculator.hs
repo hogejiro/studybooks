5eba6506de543232abcb00cd18d663994aef6d95
hogejiro <combatechizen2@gmail.com>
2015-01-18 23:59:17 +0900

    add calculator (expr only)

---
+++ programming_in_haskell/09/calculator.hs
@@ -0,0 +1,103 @@
+import Data.Char
+
+type Parser a = String -> [(a, String)]
+
+myreturn :: a -> Parser a
+myreturn v = \inp -> [(v, inp)]
+
+failure :: Parser a
+failure = \inp -> []
+
+item :: Parser Char
+item = \inp -> case inp of
+                [] -> []
+                (x:xs) -> [(x, xs)]
+
+parse :: Parser a -> String -> [(a, String)]
+parse p inp = p inp
+
+(>>>=) :: Parser a -> (a -> Parser b) -> Parser b
+p >>>= f = \inp -> case parse p inp of
+                    [] -> []
+                    [(v, out)] -> parse (f v) out
+
+(+++) :: Parser a -> Parser a -> Parser a
+p +++ q = \inp -> case parse p inp of
+                    [] -> parse q inp
+                    [(v, out)] -> [(v, out)]
+
+sat :: (Char -> Bool) -> Parser Char
+sat p = item >>>= \x -> if p x then myreturn x else failure
+
+lower :: Parser Char
+lower = sat isLower
+
+upper :: Parser Char
+upper = sat isUpper
+
+letter :: Parser Char
+letter = sat isAlpha
+
+alphanum :: Parser Char
+alphanum = sat isAlphaNum
+
+digit :: Parser Char
+digit = sat isDigit
+
+char :: Char -> Parser Char
+char x = sat (== x)
+
+string :: String -> Parser String
+string [] = myreturn []
+string (x:xs) = char x >>>= const (string xs) >>>= const (myreturn (x:xs))
+
+many :: Parser a -> Parser [a]
+many p = many1 p +++ myreturn []
+
+many1 :: Parser a -> Parser [a]
+many1 p = p >>>= \v -> many p >>>= \vs -> myreturn (v:vs)
+
+ident :: Parser String
+ident = lower >>>= \x -> (many alphanum) >>>= \xs -> myreturn (x:xs)
+
+nat :: Parser Int
+nat = (many1 digit) >>>= \xs -> myreturn (read xs)
+
+space :: Parser ()
+space = (many (sat isSpace)) >>>= \xs -> myreturn ()
+
+token :: Parser a -> Parser a
+token p = space >>>= const p >>>= \v -> space >>>= const (myreturn v)
+
+identifier :: Parser String
+identifier = token ident
+
+natural :: Parser Int
+natural = token nat
+
+symbol :: String -> Parser String
+symbol xs = token (string xs)
+
+natural_list :: Parser [Int]
+natural_list = symbol "[" >>>= const natural >>>= \n -> (many (symbol "," >>>= const natural)) >>>= \ns -> symbol "]" >>>= const (myreturn (n:ns))
+
+-- exercise 6
+-- Extend the parser for arithmetic expressions to support subtraction and division, based upon the following extensions to the grammar
+expr :: Parser Int
+expr = term >>>= \t -> (symbol "+" >>>= const expr >>>= \e -> myreturn (t + e)) +++
+                       (symbol "-" >>>= const expr >>>= \e -> myreturn (t - e)) +++
+                       myreturn t
+
+term :: Parser Int
+term = factor >>>= \f -> (symbol "*" >>>= const term >>>= \t -> myreturn (f   *   t)) +++
+                         (symbol "/" >>>= const term >>>= \t -> myreturn (f `div` t)) +++
+                         myreturn f
+
+-- exercise 7
+-- Further extend the grammar and parser for arithmetic expressions to support exponentiation, which is assumed to associate to the right and have higher priority than multiplication and division, but lower priority than parentheses and numbers.
+factor :: Parser Int
+factor = unary >>>= \f -> (symbol "^" >>>= const term >>>= \t -> myreturn (f ^ t)) +++
+                          myreturn f
+
+unary :: Parser Int
+unary = (symbol "(" >>>= const expr >>>= \e -> symbol ")" >>>= const (myreturn e)) +++ natural
