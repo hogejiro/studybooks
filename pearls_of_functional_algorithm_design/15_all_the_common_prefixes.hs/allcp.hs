a094f9dde6efb88088a52a96bf43fb921b288f64
hogejiro <combatechizen2@gmail.com>
2015-03-07 02:15:03 +0900

    add allcp.xs (ref: fig 15-1)

--- pearls_of_functional_algorithm_design/15_all_the_common_prefixes.hs/allcp.hs
+++ pearls_of_functional_algorithm_design/15_all_the_common_prefixes.hs/allcp.hs
@@ -1,10 +1,22 @@
-tails [x] = [[x]]
-tails xs = xs : tails (tail xs)
+allcp xs = fst4 (until (done n) (step xs) ([n], 0, 0, 1))
+            where n = length xs
+
+done n (as, i, p, k) = k == n
+
+step xs (as, i, p, k)
+    | k >= i + p = (snoc as         a, k, a, k + 1)
+    | q /= r     = (snoc as (min q r), i, p, k + 1)
+    | q == r     = (snoc as         b, k, b, k + 1)
+        where q = as !! (k - i)
+              r = p - (k - i)
+              a = llcp xs (drop k xs)
+              b = q + llcp (drop q xs) (drop (q + k) xs)
+
+fst4 (a, b, c, d) = a
+snoc xs x = xs ++ [x]
 
 llcp xs [] = 0
 llcp [] xs = 0
-llcp (x:xs) (y:ys) = if x == y then 1 + llcp xs ys else 0
-
-allcp xs = map (llcp xs) (tails xs)
+llcp (x : xs) (y : ys) = if x == y then 1 + llcp xs ys else 0
 
 main = print $ (allcp "abacabacab")
